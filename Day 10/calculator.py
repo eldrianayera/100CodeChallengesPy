12fcbaabbc9c518220d7bc82ee73928478d5e9fe
eldrianayera <eldrianayera710@gmail.com>
2024-09-12 20:27:33 +0800

    4 operator

---
+++ Day 10/calculator.py
@@ -0,0 +1,20 @@
+
+def add(num1 , num2) :
+    return num1 + num2
+def sub(num1 , num2) :
+    return num1 - num2
+def mul(num1 , num2) :
+    return num1  num2
+def div(num1 , num2) :
+    return num1 / num2
+
+continue_calc = True
+first_number = float(input('Enter your first number !'\n))
+
+while continue_calc
+    operator = input('Select your operator \n+\n-\n*\n/\n')
+    second_number = float(input('Enter your second number !'\n))
+    result = first_number
+
+    print(first_number, operator, second_number, '=', result)
+
