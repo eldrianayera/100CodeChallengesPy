dcb3b7706b63a546dfde682f3102588fadf1b395
eldrianayera <eldrianayera710@gmail.com>
2024-09-12 20:55:45 +0800

    reccursion

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -18,17 +18,21 @@ operators = {
 }
 
 print(operators["/"](4,5))
+def calculator() :
+    continue_calc = True
 
-continue_calc = True
+    num1 = float(input('Enter your first number : \n'))
 
-num1 = float(input('Enter your first number : \n'))
+    while continue_calc :
+        num2 = float(input('Enter your second number : \n'))
+        operator = input('Enter your operator :\n+\n-\n*\n/\n')
+        result = (operators[operator](num1,num2))
+        print(num1 , operator , num2 , '=' , result)
+        num1 = result
+        is_continue = input(f'Do you want to continue with {num1} ? Y/N ?\n')
+        if is_continue == 'N' :
+             continue_calc = False
+             calculator()
 
-while continue_calc :
-    num2 = float(input('Enter your second number : \n'))
-    operator = input('Enter your operator :\n+\n-\n*\n/\n')
-    result = (operators[operator](num1,num2))
-    print(num1 , operator , num2 , '=' , result)
-    num1 = result
-    is_continue = input(f'Do you want to continue with {num1} ? Y/N ?\n')
-    if is_continue == 'N' :
-         continue_calc = False
+
+calculator()
