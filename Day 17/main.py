e247eea1d4be129877b3edae216b3ce28d91b044
eldrianayera <eldrianayera710@gmail.com>
2024-09-16 21:19:11 +0800

    add next_question method

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -8,4 +9,9 @@ for dict in question_data :
     question_answer = dict['answer']
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question() :
+    quiz.next_question()
 
