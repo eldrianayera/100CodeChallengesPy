5d263bb801da2b5a731243ae47e355cf11d5be5e
eldrianayera <eldrianayera710@gmail.com>
2024-09-17 15:07:37 +0800

    turtle can finish

--- Day 19/turtlerace.py
+++ Day 19/turtlerace.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 from turtle import Turtle , Screen
 screen = Screen()
 screen.setup(width=500 , height=400)
@@ -16,6 +17,8 @@ turtle_member = [t1 , t2, t3, t4, t5, t6]
 index = 0
 start_x = -220
 start_y = -120
+finish_x = 220
+
 for turtle in turtle_member :
     turtle.color(colors[index])
     index += 1
@@ -24,12 +27,30 @@ for turtle in turtle_member :
     turtle.teleport(start_x , start_y)
     start_y += 50
 
-def back() :
-    t1.backward(10)
-    print(t1.pos())
+def check_finish():
+    for turtle in turtle_member :
+        print(turtle.pos()[0])
+        if turtle.pos()[0] > 220 :
+            return True
+
+def start_the_race() :
+    race_on = True
+    while race_on :
+        if check_finish() :
+            race_on = False
+
+        else :
+            for turtle in turtle_member :
+                screen.ontimer(turtle.forward(random.randint(10,30)) , 100)
+
+
+
+
+
 
 screen.listen()
-screen.onkey(fun=back , key='s')
+screen.onkey(fun=start_the_race , key='space')
 
 
 screen.exitonclick()
+
