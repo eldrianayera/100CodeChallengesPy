d768c70a4405d218a59d0548255ec4431e82570d
Eldrian <eldrianayera710@gmail.com>
2024-09-24 21:33:11 +0800

    update scoreboard.py

--- Day 20/scoreboard.py
+++ Day 20/scoreboard.py
@@ -7,4 +7,7 @@ class Scoreboard(Turtle) :
         super().__init__() 
         self.score = 0
         self.color('white')
+        self.hideturtle()
+        self.penup()
+        self.goto(0,270)
         self.write(f"Score: {self.score}" , align="center", font=("Arial",24,'normal'))
