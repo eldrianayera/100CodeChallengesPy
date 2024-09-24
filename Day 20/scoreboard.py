65e4bca2172953339ae78eb1ca21adb37fe291bf
Eldrian <eldrianayera710@gmail.com>
2024-09-24 21:37:57 +0800

    update scoreboard.increase_score

--- Day 20/scoreboard.py
+++ Day 20/scoreboard.py
@@ -10,9 +10,13 @@ class Scoreboard(Turtle) :
         self.hideturtle()
         self.penup()
         self.goto(0,270)
-        self.write(f"Score: {self.score}" , align="center", font=("Arial",24,'normal'))
+        self.update_scoreboard()
 
+    def update_scoreboard(self) :
+        self.write(f"Score: {self.score}" , align="center", font=("Arial",24,'normal'))
+        
     def increase_score(self):
         self.score += 1
-        self.write(f"Score: {self.score}" , align="center", font=("Arial",24,'normal'))
+        self.update_scoreboard()
+
         
