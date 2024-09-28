b78746aaa0de39d97d6af022936b81e4bf6c1069
Eldrian <eldrianayera710@gmail.com>
2024-09-28 15:02:18 +0800

    add snake extend

--- Day 20/snake_game.py
+++ Day 20/snake_game.py
@@ -40,4 +40,9 @@ while game_is_on:
         game_is_on = False 
         scoreboard.game_over()
 
+    for segment in snake.segments:
+        if snake.head.distance(segment) < 10 :
+            game_is_on= False
+            scoreboard.game_over()
+
 screen.exitonclick()
