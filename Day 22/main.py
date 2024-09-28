4a6ae12ea670c411b1b31be01d8a4bbb64ee89b9
Eldrian <eldrianayera710@gmail.com>
2024-09-28 16:56:15 +0800

    day22: add paddle

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,8 +1,13 @@
-from turtle import Screen
+from turtle import Screen ,Turtle
 
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800,height=600)
+
+paddle = Turtle()
+paddle.shape('square')
+paddle.color('white')
+paddle.shapesize(stretch_len=1,stretch_wid=5)
 
 
 screen.exitonclick()
