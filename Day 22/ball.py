94b2aaf2b6fa3f6a062efdaf4838a5be32e9f42e
Eldrian <eldrianayera710@gmail.com>
2024-09-28 17:30:19 +0800

    day22: move ball

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -7,4 +7,10 @@ class Ball(Turtle) :
         super().__init__()
         self.shape('circle')
         self.color('white')
-        self.goto(0,0)
+        self.penup()
+
+    def move(self) :
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x,new_y)
+        
