36300b55fc10a5a75f21dc45323746716fdfecf9
Eldrian <eldrianayera710@gmail.com>
2024-09-28 14:51:33 +0800

    add game over

--- Day 20/scoreboard.py
+++ Day 20/scoreboard.py
@@ -15,6 +15,9 @@ class Scoreboard(Turtle) :
 
     def update_scoreboard(self) :
         self.write(f"Score: {self.score}" , align=ALIGNMENT, font=FONT)
+    
+    def game_over(self):
+        self.write("GAME OVER" , align=ALIGNMENT , font=FONT)
         
     def increase_score(self):
         self.score += 1
