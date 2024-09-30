8f3fc45a6195ce4c6f299e76fa9e7842482c52fa
Eldrian <eldrianayera710@gmail.com>
2024-09-30 20:59:28 +0800

    snake game : add high score

--- Day 20/scoreboard.py
+++ Day 20/scoreboard.py
@@ -15,21 +15,23 @@ class Scoreboard(Turtle) :
         self.high_score = 0
 
     def update_scoreboard(self) :
+        self.clear()
         self.write(f"Score: {self.score}" , align=ALIGNMENT, font=FONT)
 
     def reset(self) :
         if self.score > self.high_score :
             self.high_score = self.score
+        self.score = 0
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER" , align=ALIGNMENT , font=FONT)
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER" , align=ALIGNMENT , font=FONT)
         
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
-    def 
+    
 
         
