6668b99b41ba34ed5a5f3754f976f88035d08f06
Eldrian <eldrianayera710@gmail.com>
2024-10-01 22:53:31 +0800

    snake_game : update data.txt

--- Day 20/scoreboard.py
+++ Day 20/scoreboard.py
@@ -24,12 +24,14 @@ class Scoreboard(Turtle) :
     def reset(self) :
         if self.score > self.high_score :
             self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.high_score}')
         self.score = 0
         self.update_scoreboard()
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER" , align=ALIGNMENT , font=FONT)
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER" , align=ALIGNMENT , font=FONT)
         
     def increase_score(self):
         self.score += 1
