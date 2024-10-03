aed9c7b67c13acf6d289109ed3986eaca2e8efa6
Eldrian <eldrianayera710@gmail.com>
2024-10-03 16:57:15 +0800

    snake.game update data.txt path

--- Day 20/scoreboard.py
+++ Day 20/scoreboard.py
@@ -9,8 +9,8 @@ class Scoreboard(Turtle) :
     def __init__(self) :
         super().__init__() 
         self.score = 0
-        with open("data.txt") as data :
-            self.high_score = int(data.read())
+        with open("./Day 20/data.txt") as data :
+            self.high_score = data.read()
         self.color('white')
         self.hideturtle()
         self.penup()
--- Day 20/snake_game.py
+++ Day 20/snake_game.py
@@ -28,7 +28,7 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
     
-
+ 
     #detect collision 
     if snake.head.distance(food) < 15 :
         food.refresh()
