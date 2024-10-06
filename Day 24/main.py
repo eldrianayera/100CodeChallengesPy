82ab3a2c2293a608bc7f9d5dbf64a718b34476e2
Eldrian <eldrianayera710@gmail.com>
2024-10-06 10:40:58 +0800

    mail merge : letter path

--- Day 24/main.py
+++
@@ -1,5 +0,0 @@
-with open('./Day 24/data.txt') as data :
-    go = data.read(data.aku)
-    print(go)
-
-
---
+++ Day 24/output/main.py
@@ -0,0 +1,5 @@
+with open('./Day 24/input/Letters/starting_letter.txt') as letter :
+    go = letter.read()
+    print(go)
+
+
