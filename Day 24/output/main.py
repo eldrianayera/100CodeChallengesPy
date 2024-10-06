c571f2ac32c4159286b6ebf7a706f70ce927b976
Eldrian <eldrianayera710@gmail.com>
2024-10-06 10:43:13 +0800

    mail merge : invited path

--- Day 24/output/main.py
+++ Day 24/output/main.py
@@ -2,8 +2,8 @@ with open('./Day 24/input/Letters/starting_letter.txt') as letter :
     letter = letter.read()
     print(letter)
 
-with open('./Day 24/input/Letters/starting_letter.txt') as letter :
-    invited = letter.read()
+with open('./Day 24/input/Names/starting_letter.txt') as invited :
+    invited = invited.read()
     print(invited)
 
 
