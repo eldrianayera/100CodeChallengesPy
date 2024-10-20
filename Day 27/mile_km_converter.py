eaa4c282d5e193fc5599bd1363bdbea981ceb574
Eldrian <eldrianayera710@gmail.com>
2024-10-20 14:05:30 +0800

    mile km converter : update miles

--- Day 27/mile_km_converter.py
+++ Day 27/mile_km_converter.py
@@ -5,7 +5,9 @@ window.title('Mile to Km converter')
 window.minsize(width=500,height=500)
 
 miles_input = Entry()
-miles_input.grid(column=1 , row=2)
+miles_input.grid(column=1 , row=0)
 
+miles_label = Label(text= 'Miles')
+miles_label.grid(column=2 , row=0)
 
 window.mainloop()
