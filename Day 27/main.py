7b9b1642661e436d452abd1d9d15ea35b093856a
Eldrian <eldrianayera710@gmail.com>
2024-10-20 13:42:25 +0800

    tkinter: add button GUI

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,21 +1,27 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title('My First GUI Program')
 window.minsize(width = 500 , height= 500)
 
 #Label
 
-my_label = tkinter.Label(text='I Am a Label', font=('Arial' , 24 , 'bold'))
-my_label.pack(side='left')
+my_label = Label(text='I Am a Label', font=('Arial' , 24 , 'bold'))
+my_label.pack()
 
 # my_label['text'] = 'New Label Text'
 my_label.config(text='New Text' , font =('Arial' , 25 , 'bold'))
 
 
+# Button
 
+def button_clicked():
+    print('I got clicked')
+    if my_label
+    my_label['text'] = 'I Got Clicked !'
 
-
+button = Button(text= 'Click Me' , command = button_clicked)
+button.pack()
 
 
 
