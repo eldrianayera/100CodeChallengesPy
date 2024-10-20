dd1d38e71cbf97ff3cc457b7f1cf21597869f964
Eldrian <eldrianayera710@gmail.com>
2024-10-20 14:04:10 +0800

    mile km converter : update

--- Day 27/mile_km_converter.py
+++ Day 27/mile_km_converter.py
@@ -1,7 +1,11 @@
-from tkinter import Tk
+from tkinter import *
 
 window = Tk()
 window.title('Mile to Km converter')
 window.minsize(width=500,height=500)
+
+miles_input = Entry()
+miles_input.grid(column=1 , row=2)
+
 
 window.mainloop()
