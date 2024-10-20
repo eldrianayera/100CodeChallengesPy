ae48dcc21e5973ee03b2423e2df73ead4e5989c9
Eldrian <eldrianayera710@gmail.com>
2024-10-20 13:26:49 +0800

    tkinter: initial commit

--- Day 27/main.py
+++ Day 27/main.py
@@ -4,5 +4,17 @@ window = tkinter.Tk()
 window.title('My First GUI Program')
 window.minsize(width = 500 , height= 500)
 
+#Label
+
+my_label = tkinter.Label(text='I Am a Label')
+
+
+
+
+
+
+
+
+
 
 window.mainloop()
